07e8205d84ce9ffaf8243ffd5c55c96f2821c67b
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-12-05 15:42:28 +0530

    Delete Ex2.Py

--- Ex2.Py
+++
@@ -1,8 +0,0 @@
-("print How old are you?"),
-age=raw_input()
-("print how tall ar you?"),
-height= raw_input()
-("print how much do you weight?"),
-weight= raw_input()
-
-("print so you are %r old, %r tall and %r heavy.")(age ,height,weight)
